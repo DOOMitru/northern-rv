92b0b1892f5d490c901da4556ebb321e95252b26
DOOMitru <dumitru.campan@gmail.com>
2025-01-18 13:00:21 -0600

    Re-work header

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,15 +1,15 @@
-const sidebar = document.querySelector('.sidebar');
+// const sidebar = document.querySelector('.sidebar');
 
-function showSideBar(e) {
-	sidebar.style.display = 'flex';
-}
+// function showSideBar(e) {
+// 	sidebar.style.display = 'flex';
+// }
 
-function hideSideBar(e) {
-	sidebar.style.display = 'none';
-}
+// function hideSideBar(e) {
+// 	sidebar.style.display = 'none';
+// }
 
-const showSidebarButton = document.querySelector('.show-sidebar-button');
-const hideSidebarButton = document.querySelector('.hide-sidebar-button');
+// const showSidebarButton = document.querySelector('.show-sidebar-button');
+// const hideSidebarButton = document.querySelector('.hide-sidebar-button');
 
-showSidebarButton.addEventListener('click', showSideBar);
-hideSidebarButton.addEventListener('click', hideSideBar);
+// showSidebarButton.addEventListener('click', showSideBar);
+// hideSidebarButton.addEventListener('click', hideSideBar);
